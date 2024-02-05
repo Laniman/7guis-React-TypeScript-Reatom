cd9af90bff6d3341acd21c2b89b44bbaff082769
George Achkasov <georgij-net@yandex.ru>
2024-02-05 15:28:01 +0400

    replace mobx with reatom in FlightBooker

--- src/app/guis/flight.tsx
+++ src/app/guis/flight.tsx
@@ -1,120 +1,88 @@
-import * as React from 'react'
-import {Component} from 'react'
-import {observer} from 'mobx-react'
-import {computed, IComputedValue, IObservableValue, observable} from 'mobx'
-import {DateTime} from 'luxon'
-import {Button, TextInput, VFlex} from '../basic'
+import React, { Component } from 'react';
+import { DateTime } from 'luxon';
+import { reatomComponent, useAtom } from '@reatom/npm-react';
+import { Button, TextInput, VFlex } from '../basic';
 
-const dateFormat = 'dd.MM.yyyy'
+const dateFormat = 'dd.MM.yyyy';
 
 function getTimestamp(date: string): number {
-  const parsed = DateTime.fromFormat(date, dateFormat)
-  if (parsed.invalid != null) return null
-  return parsed.valueOf()
+  const parsed = DateTime.fromFormat(date, dateFormat);
+  if (parsed.invalid != null) return null;
+  return parsed.valueOf();
 }
 
 function isValidDate(date: string): boolean {
-  return getTimestamp(date) != null
+  return getTimestamp(date) != null;
 }
 
-// TODO: How to make themeGet('space.1') work?
-@observer
-export class FlightBooker extends Component {
+export const FlightBooker = reatomComponent(({ ctx }) => {
+  const [type, setType, typeAtom] = useAtom<'one-way' | 'return'>('one-way');
 
-  @observable type: 'one-way' | 'return' = 'one-way'
+  const initDate = DateTime.local().toFormat(dateFormat);
+  const [start, setStart, startAtom] = useAtom(initDate);
+  const [end, setEnd, endAtom] = useAtom(initDate);
 
-  readonly start = observable.box(DateTime.local().toFormat(dateFormat))
-  readonly end = observable.box(this.start.get())
-  readonly validStart = computed(() => isValidDate(this.start.get()))
-  readonly validEnd = computed(() => isValidDate(this.end.get()))
-  readonly disabledEnd = computed(() => this.type !== 'return')
+  const [validStart, , validStartAtom] = useAtom((ctx) => isValidDate(ctx.spy(startAtom)));
+  const [validEnd, , validEndAtom] = useAtom((ctx) => isValidDate(ctx.spy(endAtom)));
 
-  @computed get bookable(): boolean {
-    if (!this.validStart.get() || !this.validEnd.get()) return false
-    if (this.type === 'return') {
-      return getTimestamp(this.start.get()) <= getTimestamp(this.end.get())
+  const disabledEnd = type !== 'return';
+
+  const [bookable] = useAtom((ctx) => {
+    if (!ctx.spy(validStartAtom) || !ctx.spy(validEndAtom)) return false;
+    if (ctx.spy(typeAtom) === 'return') {
+      return getTimestamp(ctx.spy(startAtom)) <= getTimestamp(ctx.spy(endAtom));
     }
-    return true
-  }
+    return true;
+  });
 
-  handleBook = () => {
-    switch (this.type) {
-      case 'one-way': return alert(`You have booked a one-way flight for ${this.start.get()}`)
-      case 'return': return alert(`You have booked a return flight from ${this.start.get()} to ${this.end.get()}`)
+  const handleBook = () => {
+    switch (ctx.get(typeAtom)) {
+      case 'one-way':
+        return alert(`You have booked a one-way flight for ${ctx.get(startAtom)}`);
+      case 'return':
+        return alert(
+          `You have booked a return flight from ${ctx.get(startAtom)} to ${ctx.get(endAtom)}`,
+        );
     }
-    throw 'Impossible'
-  }
+    throw 'Impossible';
+  };
 
-  render() {
-    return (
-      <VFlex
-        minWidth='200px'
-        vspace={2}
-      >
-        <select
-          value={this.type}
-          onChange={(e) => this.type = e.target.value as any}
-        >
-          <option value='one-way'>one-way flight</option>
-          <option value='return'>return flight</option>
-        </select>
-        <DateInput
-          value={this.start}
-          valid={this.validStart}
-          onChange={(e) => this.start.set(e.target.value)}
-        />
-        <DateInput
-          value={this.end}
-          valid={this.validEnd}
-          disabled={this.disabledEnd}
-          onChange={(e) => this.end.set(e.target.value)}
-        />
-        <Button
-          disabled={!this.bookable}
-          onClick={this.handleBook}
-        >
-          Book
-        </Button>
-      </VFlex>
-    )
-  }
-}
+  return (
+    <VFlex minWidth="200px" vspace={2}>
+      <select value={type} onChange={(e) => setType(e.target.value as 'one-way' | 'return')}>
+        <option value="one-way">one-way flight</option>
+        <option value="return">return flight</option>
+      </select>
+      <DateInput value={start} valid={validStart} onChange={(e) => setStart(e.target.value)} />
+      <DateInput
+        value={end}
+        valid={validEnd}
+        disabled={disabledEnd}
+        onChange={(e) => setEnd(e.target.value)}
+      />
+      <Button disabled={!bookable} onClick={handleBook}>
+        Book
+      </Button>
+    </VFlex>
+  );
+});
 
-/*
-Performance: Only this component will be rerendered in certain situations.
-Enable 'Visualize component rerenders' of MobX DevTools (bottom right corner in
-live version) to see the effect.
-
-That's why I don't use @observable/@computed for some fields. Proxies could make
-this transparent, I think.
-
-In general, it'd be cool if you needn't be aware of that and that extracting
-dedicated components wouldn't be necessary either but would be done
-automatically as an optimization. This would probably require a different
-language.
-
-Note that this performance optimization is not needed it is just here for
-illustrative purposes.
-*/
-
-@observer
 class DateInput extends Component<{
-  value: IObservableValue<string>
-  valid: IComputedValue<boolean>
-  disabled?: IComputedValue<boolean>
-  onChange: AnyListener
+  value: string;
+  valid: boolean;
+  disabled?: boolean;
+  onChange: AnyListener;
 }> {
-
   render() {
-    const { value, valid, disabled, onChange } = this.props
-    const d = disabled != null && disabled.get()
+    const { value, valid, disabled, onChange } = this.props;
+    const d = disabled != null && disabled;
     return (
       <TextInput
-        value={value.get()}
+        value={value}
         disabled={d}
-        style={{background: !valid.get() && !d ? 'coral' : undefined}}
+        style={{ background: !valid && !d ? 'coral' : undefined }}
         onChange={onChange}
       />
-    )
+    );
   }
 }
