da9fa58af8c110a8a238d66eb59f810777e91bc2
George Achkasov <georgij-net@yandex.ru>
2024-02-11 02:51:14 +0400

    replace with module.css + tailwind

--- src/app/basic.tsx
+++ src/app/basic.tsx
@@ -168,55 +168,6 @@ export const Label2 = ({ className, ...rest}: React.HTMLProps<HTMLSpanElement>) 
   return <Span2 className={cx(['p-1', 'text-center', 'min-w-[1rem]'], className)} {...rest} />
 }
 
-// TODO: How to merge with an existing classname?
-
-export const Button = styled(BoxClickable.withComponent(tag.button))`
-// TODO: Why is this necessary?
-${space} 
-${width} ${height}
-${minWidth} ${maxWidth} ${minHeight} ${maxHeight}
-${fontSize}
-${fontWeight}
-${color} 
-${flex}
-${textAlign}
-${background};
-
-// https://gist.github.com/fredsted/7147450
-text-decoration: none;
-color: black;
-font-size: 11px;
-border:1px solid #9C9C9C;
-display: inline-block;
-background-image: -webkit-linear-gradient(
-#ffffff 0%, #F6F6F6 	30%, 
-#F3F3F3 45%, #EDEDED 	60%, 
-#eeeeee 100%);
-border-radius: 3px;
-cursor: default;
-box-shadow: 0px 0px 1px rgba(0,0,0,0.20);
-
-&:active {
-  border-color:#705ebb;
-  background-image:-webkit-linear-gradient(
-  #acc5e9 0%, 		#a3c0f2 18%, 
-  #61a0ed 39%,		#55a3f2 70%, 
-  #82c2f1 91.72%, 	#9AD2F2 100%); 
-  box-shadow: 0px 0px 1px rgba(0,0,0,0.65);		
-}
-
-&:disabled {
-  color: #999!important;
-  background-image: -webkit-linear-gradient(#fbf8f8 0%, #f0f0f0 30%, #e3e3e3 45%, #d7d7d7 60%, #cbc9c9 100%);
-}
-` as any
-Button.defaultProps = {
-  p: 1, px: 2,
-  bg: '#ccc',
-  textAlign: 'center',
-  minWidth: '1rem',
-}
-
-export const Button2 = ({ className, ...rest}) => {
-  return <button className={cx(styles.button, styles.clickable, 'py-1', 'px-2', 'bg-[#ccc]', 'text-center', 'min-w-[1rem]' , className)} {...rest} />
+export const Button = ({ className, ...rest}: React.ComponentPropsWithoutRef<'button'>) => {
+  return <button type="button" className={cx(styles.button, styles.clickable, 'py-1', 'px-2', 'bg-[#ccc]', 'text-center', 'min-w-[1rem]' , className)} {...rest} />
 }
--- src/app/guis/counter.tsx
+++ src/app/guis/counter.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { atom } from '@reatom/framework';
 import { reatomComponent } from '@reatom/npm-react';
-import { Button2, Flex2, Label2 } from '../basic';
+import { Button, Flex2, Label2 } from '../basic';
 import { cx } from '../utils';
 
 const countAtom = atom(0);
@@ -9,8 +9,8 @@ const countAtom = atom(0);
 export const Counter = reatomComponent(({ ctx }) => (
   <Flex2 className={cx('items-center', 'min-w-[200px]')}>
     <Label2 className="flex-1">{ctx.spy(countAtom)}</Label2>
-    <Button2 className="flex-1" onClick={() => countAtom(ctx, (count) => count + 1)}>
+    <Button className="flex-1" onClick={() => countAtom(ctx, (count) => count + 1)}>
       Count
-    </Button2>
+    </Button>
   </Flex2>
 ));
